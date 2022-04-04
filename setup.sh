c4fb566c2d5495d2653109a11174661ae139395e
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2022-04-04 10:38:00 +0200

    updated setup.sh + session9 notebook

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 pip install --upgrade pip
-pip install spacy tqdm vaderSentiment networkx
+pip install spacy tqdm spacytextblob vaderSentiment networkx scikit-learn tensorflow gensim
 python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_md
