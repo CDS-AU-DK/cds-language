64512432cf828d279545d63e7af08fd5865dfb23
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2023-01-30 21:24:52 +0100

    first push of '23

--- setup.sh
+++ setup.sh
@@ -1,6 +1,1 @@
 #!/usr/bin/env bash
-
-pip install --upgrade pip
-pip install spacy tqdm spacytextblob vaderSentiment networkx scikit-learn tensorflow gensim
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_md
