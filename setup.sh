8742d8b229b30cad8c39a44548312698aa08cb8b
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2024-02-28 20:12:54 +0100

    session 5 materials

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# install hdbscan for BERTopic
-sudo apt-get update
-sudo apt-get install python3-dev
-
-# requirements
+# install requirements
 pip install --upgrade pip
-pip install --upgrade nbformat
 python3 -m pip install -r requirements.txt
