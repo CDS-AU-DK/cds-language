62acbc27220b0317c736aeb324967dd0ce7bd68b
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2024-03-15 14:51:13 +0100

    session 7 materials

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# activate the environment
+source ./env/bin/activate
+# run the code
+python src/test.py
+# close the environment
+deactivate
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
+# create virtual env
+python -m venv env
+# activate env
+source ./env/bin/activate
 # install requirements
 pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
+# close the environment
+deactivate
